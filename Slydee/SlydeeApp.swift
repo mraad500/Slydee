234b9205f187d4b72e5e353fab2770c679891fac
mraad500 <mraad500@gmail.com>
2026-05-17 07:09:09 +0400

    Initial Commit

---
+++ Slydee/SlydeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlydeeApp.swift
+//  Slydee
+//
+//  Created by Mohammed Raad on 17/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct SlydeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
